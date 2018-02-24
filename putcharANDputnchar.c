7a44e2a0ea80a57daf0ff30914724f22c850f6fe
Northbert <robert.f.dobre@gmail.com>
2018-02-24 20:38:18 +0200

    ok

--- putcharANDputnchar.c
+++ putcharANDputnchar.c
@@ -1,7 +1,6 @@
 /* this is the 3rd example fom jour 02 */
 
 #include	<unistd.h>
-#include	<stdio.h>
 
 int	ft_putchar (char c)
 {
@@ -9,22 +8,22 @@ int	ft_putchar (char c)
 	return(0);
 }
 
-/*int	ft_nputchar(char c, int n)
+int	ft_nputchar(char c, int n)
 {
 	int i;
 
 	i = 0;
-	while (1 < 3)
+	while (i < n)
 		{
 			ft_putchar(c);
 			i = i + 1;
 		}
 	return(0);
 }
-*/
+
 int	main ()
 {
-	int i,n;
+	/*int i,n;
 
 	n = 3;
 	i = 0;
@@ -34,6 +33,8 @@ int	main ()
 		ft_putchar('R');
 		i = i + 1;
 	}
-
+	*/
+	ft_nputchar('r',3);
+	ft_putchar('\n');
 	return(0);
 }	
